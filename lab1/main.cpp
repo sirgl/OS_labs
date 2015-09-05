05053fba4e5b57b257bdda800aaac9e9c24c059c
Roman <rom6190@yandex.ru>
2015-09-05 10:42:07 +0600

    Initial commit

---
+++ lab1/main.cpp
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <iostream>
+
+using namespace std;
+
+void *childFunction(void *) {
+	for (int i = 0; i < 100; ++i) {
+		cout << "Hello, it's child!" << endl;
+	}
+}
+
+int main() {
+	pthread_t thread_id;
+	int code = pthread_create(&thread_id, NULL, childFunction, NULL);
+	if (code != 0) {
+		perror("pthread_create");
+		exit(EXIT_FAILURE);
+	}
+	for (int i = 0; i < 100; ++i) {
+		cout << "Hello, it's parent!" << endl;
+	}
+	pthread_exit(EXIT_SUCCESS);
+}
