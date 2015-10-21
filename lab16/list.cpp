5f83475797144eb52b495ce96f2b77061d66f7dc
Roman <rom6190@yandex.ru>
2015-10-22 01:26:36 +0600

    16

---
+++ lab16/list.cpp
@@ -0,0 +1,102 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "list.h"
+
+const int BUFFER_SIZE = 256;
+
+void printError(int error);
+
+List *createList() {
+	List *list = (List *) malloc(sizeof(List));
+	if(NULL == list) {
+		return NULL;
+	}
+	list->head = NULL;
+	list->size = 0;
+	int error = pthread_mutex_init(&(list->mutex), NULL);
+	if(error) {
+		free(list);
+		printError(error);
+		return NULL;
+	}
+	list->interrupted = 0;
+	return list;
+}
+
+ListEntry *createListEntry(char* string) {
+	ListEntry *listEntry = (ListEntry *) malloc(sizeof(ListEntry));
+	if(NULL == listEntry) {
+		return NULL;
+	}
+	listEntry->string = string;
+	listEntry->next = NULL;
+	return listEntry;
+}
+
+void addEntryToList(List *list, ListEntry *entry) {
+	pthread_mutex_lock(&list->mutex);
+
+	ListEntry *head = list->head;
+	list->head = entry;
+	entry->next = head;
+	list->size++;
+
+	pthread_mutex_unlock(&list->mutex);
+}
+
+void sort(List* list) {
+	pthread_mutex_lock(&list->mutex);
+
+	if (NULL == list->head) {
+		return;
+	}
+	for (ListEntry* i = list->head; NULL != i; i = i->next) {
+		for (ListEntry* j = i->next; NULL != j; j = j->next) {
+			if (0 < strcmp(i->string, j->string)) {
+				char* tmp = i->string;
+				i->string = j->string;
+				j->string = tmp;
+			}
+		}
+	}
+
+	pthread_mutex_unlock(&list->mutex);
+}
+
+void printList(List* list) {
+	pthread_mutex_lock(&list->mutex);
+
+	printf("size = %d\n", list->size);
+	ListEntry *current = list->head;
+	while (NULL != current) {
+		printf("%s -> ", current->string);
+		current = current->next;
+	}
+	printf("\n");
+
+	pthread_mutex_unlock(&list->mutex);
+}
+
+void freeList(List* list) {
+	pthread_mutex_t mutex = list->mutex;
+	pthread_mutex_lock(&list->mutex);
+
+	ListEntry* entry = list->head;
+	ListEntry* toDelete;
+	while (NULL != entry) {
+		toDelete = entry;
+		entry = entry->next;
+		free(toDelete);
+	}
+	list->interrupted = 1;
+
+	pthread_mutex_unlock(&list->mutex);
+
+}
+
+void printError(int error) {
+	char buffer[BUFFER_SIZE];
+	strerror_r(error, buffer, sizeof buffer);
+	fprintf(stderr, "%s\n", buffer);
+}
--- lab7/main.cpp
+++ lab7/main.cpp
@@ -15,7 +15,13 @@ struct Parameter {
 void *calc(void *param) {
 	Parameter *parameter = (Parameter *) param;
 	double result = 0;
-	for (int i = parameter->index; i < ITERATIONS; i += threadCount) {
+//	for (int i = parameter->index; i < ITERATIONS; i += threadCount) {
+//		result += 1.0 / (i * 4.0 + 1.0);
+//		result -= 1.0 / (i * 4.0 + 3.0);
+//	}
+	int start = ITERATIONS / threadCount * parameter->index ;
+	int end = parameter->index == threadCount - 1 ? ITERATIONS : ITERATIONS / threadCount * (parameter->index + 1);
+	for (int i = start; i < end; ++i) {
 		result += 1.0 / (i * 4.0 + 1.0);
 		result -= 1.0 / (i * 4.0 + 3.0);
 	}
@@ -26,7 +32,7 @@ void *calc(void *param) {
 int main(int argc, char **argv) {
 	if (2 != argc) {
 		fprintf(stderr, "Not enough arguments");
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
 	threadCount = atoi(argv[1]);
@@ -35,13 +41,13 @@ int main(int argc, char **argv) {
 	Parameter **parameters = (Parameter **) malloc(sizeof(Parameter *) * threadCount);
 	if (NULL == parameters) {
 		fprintf(stderr, "Not enough memory");
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 	for (int j = 0; j < threadCount; ++j) {
 		parameters[j] = (Parameter *) malloc(sizeof(Parameter));
 		if (NULL == parameters[j]) {
 			fprintf(stderr, "Not enough memory");
-			return EXIT_FAILURE;
+			exit(EXIT_FAILURE);
 		}
 		parameters[j]->index = j;
 		parameters[j]->result = 0;
@@ -52,7 +58,7 @@ int main(int argc, char **argv) {
 		int error = pthread_create(&threads[i], NULL, calc, parameters[i]);
 		if (error) {
 			fprintf(stderr, "error while creating threads");
-			return EXIT_FAILURE;
+			exit(EXIT_FAILURE);
 		}
 	}
 
@@ -63,7 +69,7 @@ int main(int argc, char **argv) {
 		int error = pthread_join(threads[i], (void **) &retValue);
 		if (error) {
 			fprintf(stderr, "error while creating threads");
-			return EXIT_FAILURE;
+			exit(EXIT_FAILURE);
 		}
 		pi += retValue->result;
 		free(retValue);
