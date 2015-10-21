99952d43d4fe36672ca2aa8f6081a5ab62d407c5
Roman <rom6190@yandex.ru>
2015-10-22 01:46:17 +0600

    19

---
+++ lab19/list.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <pthread.h>
+
+typedef struct ListEntry {
+	char* string;
+	ListEntry* next;
+} ListEntry;
+
+
+typedef struct List {
+	pthread_rwlock_t lock;
+	ListEntry* head;
+	int size;
+	int interrupted;
+} List;
+
+ListEntry * createListEntry(char* string);
+
+List * createList();
+
+void addEntryToList(List *list, ListEntry *entry);
+
+void sort(List* list);
+
+void printList(List* list);
+
+void freeList(List* list);
---
+++ lab19/main.cpp
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "list.h"
+
+static const int MAX_STRING_LENGTH = 80;
+
+void* childFunction(void* parameter) {
+	List* list = (List *) parameter;
+	while(!list->interrupted) {
+		sleep(5);
+		sort(list);
+	}
+	pthread_rwlock_destroy(&list->lock);
+	free(list);
+}
+
+int main(int argc, char **argv) {
+	List* list = createList();
+	if(NULL == list) {
+		fprintf(stderr, "No memory");
+		return EXIT_FAILURE;
+	}
+
+	char buffer[MAX_STRING_LENGTH + 1];
+
+	ssize_t readSize;
+	for(;;) {
+		readSize = read(0, buffer, MAX_STRING_LENGTH - 1);
+		if(0 == readSize) {
+			break;
+		}
+		buffer[readSize] = 0;
+		if(0 == strcmp(buffer, "end\n")){
+			break;
+		}
+		if(0 == strcmp(buffer, "\n")) {
+			printList(list);
+			continue;
+		}
+		char* entryString = (char *) malloc((readSize + 1) * sizeof(char));
+		if(NULL == entryString) {
+
+		}
+		strcpy(entryString, buffer);
+
+		ListEntry *entry = createListEntry(entryString);
+		if(NULL == entry) {
+			fprintf(stderr, "No memory");
+			freeList(list);
+			pthread_exit((void *) EXIT_FAILURE);
+		}
+		addEntryToList(list, entry);
+	}
+
+	freeList(list);
+	pthread_exit((void *) EXIT_FAILURE);
+
+}
